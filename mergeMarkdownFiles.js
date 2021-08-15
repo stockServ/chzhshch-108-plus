34ccfe11f31c4207f9463ffb2e48cc754aa581b0
FigoZhu <figozhu@gmail.com>
2021-08-15 09:44:11 +0800

    补充了疑似原图后的版本

---
+++ mergeMarkdownFiles.js
@@ -0,0 +1,29 @@
+
+const fs = require('fs');
+const path = require('path');
+
+const mdFileList = fs.readdirSync('./108');
+// console.log(mdFileList);
+
+let mdAllContent = "[toc]\r\n";
+let mdArticleContent = "[toc]\r\n";
+
+for (const szMDFileName of mdFileList) {
+    if (path.extname(szMDFileName) !== '.md') {
+        continue;
+    }
+    const realFileName = `./108/${szMDFileName}`;
+    // console.log(realFileName);
+
+    const oneMDContent = fs.readFileSync(realFileName, 'utf-8');
+    const artileMDContent = oneMDContent.split("**本文评论获取自")[0];
+
+    // 全部
+    mdAllContent += oneMDContent + "\r\n\r\n";
+
+    // 只包含课文
+    mdArticleContent += artileMDContent + "\r\n\r\n";
+}
+
+fs.writeFileSync("./108/108-All.md", mdAllContent);
+fs.writeFileSync("./108/108-artileOnly.md", mdArticleContent);
